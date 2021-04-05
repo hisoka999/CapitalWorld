39c2d68d0c42f9e6a1014dc0b2c51323f5ad0193
hisoka999 <hisoka999@gmail.com>
2021-04-05 20:56:54 +0200

    removed boost from the dependencies

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-gfx-dev libsdl2-mixer-dev libz-dev libboost1.65-dev
+sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-gfx-dev libsdl2-mixer-dev libz-dev
