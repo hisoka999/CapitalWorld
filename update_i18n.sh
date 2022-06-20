e19ad0b3ab18931b0e6a2c8d943a3bf5d02500b7
hisoka999 <hisoka999@gmail.com>
2022-06-20 20:55:57 +0200

    updated german translation

--- update_i18n.sh
+++ update_i18n.sh
@@ -3,5 +3,5 @@
 for file in $(find src/ -name '*.cpp')
 do
     echo $file
-    xgettext --keyword=_ --language=C++ --add-comments -j --sort-output -o locale/capitalworld.pot $file
+    xgettext --keyword=_ --language=C++ --add-comments -j --from-code="utf-8" --sort-output -o locale/capitalworld.pot $file
 done
